8fb86ec939c8cbe790c8f8af5034fd61ddb18524
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-29 23:27:05 -0400

    Wrote tests for hobbitsModel

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,1 +1,26 @@
-test.todo("write these tests");
+const db = require("../data/dbConfig");
+const Hobbits = require("./hobbitsModel");
+
+describe("hobbits model", () => {
+	describe("insert()", () => {
+		it("should insert hobbits into db", async () => {
+			await Hobbits.insert({ name: "daniel" });
+			await Hobbits.insert({ name: "sam" });
+
+			const addedHobbits = await db("hobbits");
+			expect(addedHobbits).toHaveLength(2);
+		});
+
+		it("should add a single hobbit", async () => {
+			let hobbit = await Hobbits.insert({ name: "gaffer" });
+			expect(hobbit.name).toBe("gaffer");
+
+			hobbit = await Hobbits.insert({ name: "sam" });
+			expect(hobbit.name).toBe("sam");
+		});
+	});
+});
+
+beforeEach(async () => {
+	await db("hobbits").truncate();
+});
